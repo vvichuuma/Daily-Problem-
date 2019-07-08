72bbe530185ab436e4b12e840025b28bf47b2c12
VISHNU <vvichugit@gmail.com>
2019-07-08 12:09:24 -0500

    solution to week - 9

--- week_8.js
+++ week_8.js
@@ -1,1 +1,106 @@
 console.log("This is the Week 8 solution");
+// 1).var array = [["a", "b", "z"], ["c", "d"], ["e", "f"], ["g", "h", "i", "j"]];
+
+// var code = [];
+
+// array.forEach( (ar) => {
+
+//    ar.forEach((a) => {
+//      code.push(a);
+//    })
+
+// })
+
+// console.log(code);
+
+// 2).var meals = [["breakfast", "pancakes with maple syrup"], ["lunch", "BLT"], ["dinner", "salmon with lemon rice"]];
+
+// var h = {};
+
+// meals.forEach( (meal) => {
+
+//   h[meal[0]] = meal[1];
+
+// })
+
+// console.log(h);
+
+// 3) .
+
+// function findLongestWord(string){
+
+//     let words = string.split(" ");
+
+//     let w_len = 0;
+//     let wsen = "";
+
+//     words.forEach( (word) => {
+
+//        if(w_len < word.length){
+//          w_len = word.length;
+//           wsen = word;
+//        }
+
+//     })
+
+//     console.log(wsen);
+
+//   }
+
+//   findLongestWord("What is the longest word moreLonger in this phrase?");
+
+// 4). mutation:
+
+// function mutation(a , b){
+
+//     var l = 0;
+//     var bword = b.split("");
+//     let s = 0;
+
+//     for(let i = 0; i < b.length; i++){
+//        if(a.includes(bword[i])){
+//             s+=1;
+//        }
+//     }
+
+//    if(s == b.length){
+//      console.log("Yes it includes");
+//    }else{
+//      console.log("No it doesn't include");
+//    }
+
+// }
+
+// mutation("burly" , "ruby");
+// mutation("burly", "python");
+
+// 5). Most Letter in Word:
+
+// function mostA(array){
+
+//     var ans = "";
+//     var len = 0;
+
+//     array.forEach( (ar) => {
+
+//       var s = 0;
+//       var words = ar.split("");
+
+//           for(let i = 0; i < words.length; i++){
+//               if(words[i] == 'a'){
+//                   s+=1;
+//               }
+//           }
+
+//           if(len < s){
+//             len = s;
+//             ans = ar;
+//           }
+
+//     })
+
+//     console.log("The word is " + ans + " and the number of a's is " + len);
+
+//   }
+
+//   mostA(["alphabet", "aardvark", "aaaaa" ,"anarchy"])
---
+++ week_9.js
@@ -0,0 +1,54 @@
+// console.log("This is the Week 9 solution");
+
+// 1).var numbers = [1, 2, 4, 2];
+
+// var code = numbers.map(number => number * 2);
+
+// console.log(code);
+
+// 2).
+// var items = [
+//   { id: 1, body: "foo" },
+//   { id: 2, body: "bar" },
+//   { id: 3, body: "foobar" }
+// ];
+
+// let code = items.map(item => item.id);
+
+// console.log(code);
+
+// 3).
+
+// var numbers = [1, 2, 4, 2];
+
+// let code = numbers.filter(num => num % 2 == 0);
+
+// console.log(code);
+
+//4).
+
+// var movies = [
+//   { id: 1, title: "Die Hard", rating: 4.0 },
+//   { id: 2, title: "Bad Boys", rating: 5.0 },
+//   { id: 3, title: "The Chamber", rating: 3.0 },
+//   { id: 4, title: "Fracture", rating: 2.0 }
+// ];
+
+// let code = movies
+//   .filter(movie => movie.title.includes("b") || movie.title.includes("B"))
+//   .map(c => c.id);
+
+// console.log(code);
+
+// 5).
+
+// var movies = [
+//   { id: 1, title: "Die Hard", rating: 4.0 },
+//   { id: 2, title: "Bad Boys", rating: 5.0 },
+//   { id: 3, title: "The Chamber", rating: 3.0 },
+//   { id: 4, title: "Fracture", rating: 2.0 }
+// ];
+
+// let code = movies.filter(movie => movie.rating < 4.0);
+
+// console.log(code);
