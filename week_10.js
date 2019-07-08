e80123ebf1c4a1d30033a10c428e63985e8706d0
VISHNU <vvichugit@gmail.com>
2019-07-08 12:20:25 -0500

    added the solution to week 10

---
+++ week_10.js
@@ -0,0 +1,63 @@
+console.log("Week 10 solution");
+
+// 1).
+
+// var numbers = [1, 2, 4, 2];
+
+// let code = numbers.reduce((acc, n) => (acc += n));
+
+// console.log(code);
+
+//2).
+
+// let h = {};
+
+// var fruits = [
+//   { name: "apple", color: "red" },
+//   { name: "banana", color: "yellow" },
+//   { name: "grape", color: "purple" }
+// ];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   h[fruits[i]["name"]] = fruits[i]["color"];
+// }
+
+// console.log(h);
+
+//3).
+
+// people = [
+//   { name: "Saron", age: 34 },
+//   { name: "Majora", age: 28 },
+//   { name: "Danilo", age: 45 }
+// ];
+
+// let code = people.sort(function(a, b) {
+//   return a.age - b.age;
+// });
+
+// console.log(code);
+
+//4).
+
+//5).
+
+// var items = [
+//   {
+//     title: "NoDBA",
+//     words: 561,
+//     tags: ["nosql", "people", "orm"],
+//     type: "book"
+//   },
+//   { title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: "book" },
+//   { title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: "book" },
+//   { title: "ruby", words: 1313, tags: ["ruby"], type: "article" },
+//   { title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: "book" }
+// ];
+
+// let code = items
+//   .filter(item => item.type == "book")
+//   .map(item => item.words)
+//   .reduce((acc, n) => (acc += n));
+
+// console.log(code);
